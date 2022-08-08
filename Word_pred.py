0b08f46730dec2993b8b5f3d655fbf76210cd8c6
Neenu Markose <88011289+neenumarkose@users.noreply.github.com>
2022-08-08 17:17:46 -0400

    Update Word_pred.py

--- Word_pred.py
+++ Word_pred.py
@@ -129,12 +129,6 @@ if authentication_status:
 
   if st.button('Predict'):
       st.text_area("Predicted List is Here", answer_as_string, key="predicted_list")
-    
-  word_used = []
-  for i in answer_as_string:
-    answer.append(i+1)
-  word_selected = "    ".join(word_used)
-  selected_words = st.text_area ("Your selected list is..", word_selected, key="full_sentence")
       
   st.image('https://imageio.forbes.com/blogs-images/cognitiveworld/files/2019/06/types-of-AI.jpg?format=jpg&width=960',use_column_width=True)
 
